8e601b24f0aa209ad2c8995ddef42f1004444191
Hannah <hfhealy@gmail.com>
2022-04-05 15:44:10 -0700

    First commit

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+$(document).ready(() => {
 // write your code here
-
+    $.getJSON('./data.json', (data) => {
+        data.forEach(topSpot => {
+            $( ".table" ).after("<tr>" + "<td>" + topSpot.name + "</td>" + "</tr>")
+        })
+    //    var items = [];
+    //    $.each( data, function (key,val) {
+    //        $( ".table" ).append("<tr>" + "<td>" + val.name + "</td>" + "</tr>")
+           
+          
+        {/* //    $( ".description" ).append("<br>" + val.description + "<br>"); */}
+    //     });        
+    // })
+    
 });
+}); 
